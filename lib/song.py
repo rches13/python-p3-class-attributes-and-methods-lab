1cbf6e3f31f443089bd89e4a7ed67cdf75a97ab5
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-09-04 08:23:43 +0300

    add class attributes and methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,27 @@
 class Song:
-    pass
+    
+    count = 0
+    genres = []
+    artists = []
+
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.genres.append(self.genre)
+        Song.artists.append(artist)
+        Song.genre_count[self.genre] = Song.genre_count.get(self.genre, 0) + 1
+        Song.artist_count[self.artist] = Song.artist_count.get(self.artist, 0) + 1
+
+    @classmethod
+    def genre_count(cls):
+        return cls.count
+    
+    @classmethod
+    def artist_count(cls):
+        return cls.count
+    
+
+    
